429276f4a82c89189e030fdefcfd9f69f0cc515b
B V Nandhan <nandanbv50@gmail.com>
2024-07-22 19:46:16 +0530

    First commit

---
+++ client/src/routes/homePage/homePage.jsx
@@ -0,0 +1,44 @@
+import { useContext } from "react";
+import SearchBar from "../../components/searchBar/SearchBar";
+import "./homePage.scss";
+import { AuthContext } from "../../context/AuthContext";
+
+function HomePage() {
+
+  const {currentUser} = useContext(AuthContext)
+
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+          <p>
+          Introducing our cutting-edge real estate app, designed to simplify your property search. 
+          Browse through thousands of listings with ease, from cozy apartments to luxurious estates. With intuitive filters, interactive maps, and real-time updates, finding your dream home is effortless.
+           Whether buying, renting, or exploring investment opportunities, our app puts the power of real estate at your fingertips. Download now and start discovering your next move today!
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Award Gained</h2>
+            </div>
+            <div className="box">
+              <h1>2000+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
